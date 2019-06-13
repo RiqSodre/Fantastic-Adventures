b67bd781c0cdabccf819aaac36b2494c5417e9fd
RiqSodre <riqsodre@gmail.com>
2019-06-13 19:10:42 -0300

    Alterações de Nome

---
+++ Fantastic-Adventures/Prj_The_Destiny_Sword/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj_The_Destiny_Sword
+{
+    class Dice
+    {
+        public int value;
+        public int number;
+        Random random = new Random(DateTime.Now.Millisecond);
+        
+        public Dice(){}
+
+        public Dice(int number, int value)
+        {
+            this.number = number;
+            this.value = value;
+        }
+
+        public int Roll(int number, int value)
+        {
+            int result = 0;
+            for (int i = 0; i < number; i++)
+                result += random.Next(0, value);
+            return result;
+        }
+    }
+}
---
+++ Fantastic-Adventures/Prj_The_Destiny_Sword/Frm_Inicio.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Prj_The_Destiny_Sword
+{
+    partial class Frm_Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(-2, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THE EMERALD SWORD";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(552, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Frm_Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Prj_The_Destiny_Sword.Properties.Resources.jay_epperson_pa_planeofearth;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(709, 419);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Frm_Inicio";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Emerald Sword";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
